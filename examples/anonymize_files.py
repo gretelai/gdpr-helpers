d0f9b804e2a65ff127b03134619b6d039eb7fdbe
Alexander Watson <zredlined@users.noreply.github.com>
2022-12-12 13:25:55 -0800

    add overwrite param

--- examples/anonymize_files.py
+++ examples/anonymize_files.py
@@ -1,4 +1,5 @@
 import glob
+
 from gdpr_helpers import Anonymizer
 
 search_pattern = "data/*.csv"
@@ -6,10 +7,10 @@ search_pattern = "data/*.csv"
 
 def main():
     am = Anonymizer(
-        run_mode="cloud",
-        #transforms_config="config/transform_config.yaml",
-        #synthetics_config="config/synthetics_config.yaml",
-    )
+        project_name="gdpr-workflow", 
+        run_mode="cloud", 
+        overwrite=False
+        )
 
     for dataset_path in glob.glob(search_pattern):
         am.anonymize(dataset_path=dataset_path)
