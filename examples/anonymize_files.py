d158b249787b470a4547b56d023729c97c2a3e5f
Alexander Watson <zredlined@users.noreply.github.com>
2022-12-19 10:41:06 -0800

    adding config examples

--- examples/anonymize_files.py
+++ examples/anonymize_files.py
@@ -7,10 +7,12 @@ search_pattern = "data/*.csv"
 
 def main():
     am = Anonymizer(
-        project_name="gdpr-workflow", 
-        run_mode="cloud", 
+        project_name="gdpr-workflow",
+        transforms_config="src/config/transforms_config.yaml",
+        synthetics_config="synthetics/tabular-actgan",  # use synthetics/amplify for CPU
+        run_mode="cloud",  # use `hybrid` to run locally
         overwrite=False,
-        )
+    )
 
     for dataset_path in glob.glob(search_pattern):
         am.anonymize(dataset_path=dataset_path)
